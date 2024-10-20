06ce388a956ce3720b70c46dd451c0bc22fe4c78
An Hoang <an.hoang@web.de>
2024-10-20 14:37:00 +0200

    Initial code commit, established connection between backend and frontend

---
+++ code/backend/server.ts
@@ -0,0 +1,28 @@
+/*
+//import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.get("/", (req: Request, res: Response) => {
+  // root route http://localhost:5001
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+*/
+
+import express from "express";
+const app = express();
+const port = 5001;
+console.log("Heeeeeelo");
+
+app.get("/", (req, res) => {
+  console.log("Heeelo");
+  res.send("Hello from Express!");
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
