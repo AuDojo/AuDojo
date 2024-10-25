aa1dabc6526d45e96512a1679ec70206e88f8938
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-10-25 15:48:10 +0200

    create pages folder and add routes to Homepage in main

--- code/frontend/src/main.tsx
+++ code/frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './index.css'
+import Home from './pages/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
