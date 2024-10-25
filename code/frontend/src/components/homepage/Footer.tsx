3ffb4bb1c422b11af745a917ad6eb8226c5e4b8f
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-10-25 22:48:36 +0200

    fix footer color and fix transition from header style

--- code/frontend/src/components/homepage/Footer.tsx
+++ code/frontend/src/components/homepage/Footer.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import audoLogo from '../../assets/logo-audojo.png'
-import '../../styles/homepage/Footer.css'
-import {Link} from 'react-router-dom'
+import React from "react";
+import audoLogo from "../../assets/logo-audojo.png";
+import "../../styles/homepage/Footer.css";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div className='footer-content'>
-        <div><Link to="">Impressum</Link></div>
-        <div><Link to="">Datenschutz</Link></div>
+    <div className="footer-content">
+      <div>
+        <Link className="no-underline" to="">
+          Impressum
+        </Link>
+      </div>
+      <div>
+        <Link className="no-underline" to="">
+          Datenschutz
+        </Link>
+      </div>
+      <Link className="no-underline" to="">
         <div className="footer-audojo-logo">
-            <div>designed by</div>
-            <img style={{height: "50px", borderRadius: "50%"}} src={audoLogo} alt="logo of audojo" />
+          <div>designed by</div>
+          <img style={{ height: "50px", borderRadius: "50%" }} src={audoLogo} alt="logo of audojo" />
         </div>
-
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
