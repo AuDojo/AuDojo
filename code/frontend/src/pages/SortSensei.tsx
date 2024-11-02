8f24b7706bdfb563ce9e130c2467f596a0c7b570
An Hoang <an.hoang@web.de>
2024-11-02 16:09:29 +0100

    Basic structure for mergesort

--- code/frontend/src/pages/SortSensei.tsx
+++ code/frontend/src/pages/SortSensei.tsx
@@ -1,33 +1,10 @@
-import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
-import AnimationSort from "../components/sortSensei/AnimationSort";
 import Header from "../components/sortSensei/Header";
 import "../styles/sortSensei/general.css";
 const SortSensei = () => {
-  const [stepsList, setStepsList] = useState<number[][]>([]);
-
-  useEffect(() => {
-    async function fetchStepsList() {
-      const response = await fetch("/api/sorting/mergesort", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          startArray: [7, 13, 5, 9, 10, 12, 1, 3, 2, 6, 25, 30, 40, 38, 32],
-        }),
-      });
-      const data = await response.json();
-      setStepsList(JSON.parse(data).processList);
-    }
-
-    fetchStepsList();
-  }, []);
-
   return (
     <>
       <Header />
-      <AnimationSort />
       <Outlet />
     </>
   );
