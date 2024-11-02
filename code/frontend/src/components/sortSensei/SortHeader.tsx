dd3b766dd7f6064d7f78148126db2fcdb5ba4f4d
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-11-02 18:08:05 +0100

    change header style

--- code/frontend/src/components/sortSensei/SortHeader.tsx
+++ code/frontend/src/components/sortSensei/SortHeader.tsx
@@ -1,41 +1,30 @@
-import { useState } from "react";
-// import tubsLogo from "../../assets/tu-logo.png";
+
 import audojoLogo from "../../assets/logo-audojo.png";
 import { Link } from "react-router-dom";
 import headerStyles from "../../styles/sortSensei/SortHeader.module.css";
 const HeaderNavItem = ({ to, text }: { to: string; text: string }) => {
   return (
     <Link className="Link" to={to}>
-      <div className={headerStyles["header-nav-list-item"]}>{text}</div>
+      <div className={`${headerStyles["header-nav-list-item"]} ${to.includes("mergesort") && headerStyles["header-nav-list-item-active"]}`}>{text}</div>
     </Link>
   );
 };
 const SortHeader = () => {
-  const [isOpen, setOpen] = useState<boolean>(false);
-  const toogleMenu = () => {
-    setOpen(!isOpen);
-  };
-
+  // const [active]
   return (
     <div className={headerStyles["header-container"]}>
-      <a href={"https://aud.ibr.cs.tu-bs.de"} target="_blank">
+      <Link to={"/"}>
         <div className={headerStyles["header-logo-container"]}>
           <img className={headerStyles["header-logo-image"]} src={audojoLogo} alt="audojo logo" />
-          <div className={headerStyles["header-logo-text"]}>Algorithmen und Datenstrukturen</div>
+          <div className={headerStyles["header-logo-text"]}>AUDOJO</div>
         </div>
-      </a>
+      </Link>
 
       <div className={headerStyles["header-nav"]}>
         <div className={`${headerStyles["header-nav-list"]}`}>
-          <HeaderNavItem to="https://aud.ibr.cs.tu-bs.de" text="Mergesort" />
+          <HeaderNavItem to="/sortsensei/mergesort" text="Mergesort" />
           <HeaderNavItem to="/sortsensei" text="Quicksort" />
           <HeaderNavItem to="https://aud.ibr.cs.tu-bs.de" text="Bubblesort" />
-        </div>
-
-        <div className={`${headerStyles["hamburger"]} ${isOpen ? headerStyles.change : ""}`} onClick={toogleMenu}>
-          <div className={headerStyles["bar1"]}></div>
-          <div className={headerStyles["bar2"]}></div>
-          <div className={headerStyles["bar3"]}></div>
         </div>
       </div>
     </div>
