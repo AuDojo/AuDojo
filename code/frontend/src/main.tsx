29b51884131dca91d522cfb78909ded14783a68b
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-11-02 19:18:12 +0100

    make header active

--- code/frontend/src/main.tsx
+++ code/frontend/src/main.tsx
@@ -9,6 +9,7 @@ import "./index.css";
 import Home from "./pages/Home.tsx";
 import MergeSort from "./pages/MergeSort.tsx";
 import SortSensei from "./pages/SortSensei.tsx";
+import QuickSort from "./pages/QuickSort.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
       <Route path="/" element={<Home />} />
       <Route path="sortsensei" element={<SortSensei />}>
         <Route path="mergesort" element={<MergeSort />} />
+        <Route path="quicksort" element={<QuickSort />} />
       </Route>
       {/* <Route path="sortsensei/mergesort" element={<Home />} /> */}
       {/* Nested route */}
