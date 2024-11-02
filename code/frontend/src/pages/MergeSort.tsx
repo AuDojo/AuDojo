babe513b0372f0b8238c24eff3ca180d45ae60f9
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-11-02 19:18:24 +0100

    add buttons and styles

--- code/frontend/src/pages/MergeSort.tsx
+++ code/frontend/src/pages/MergeSort.tsx
@@ -1,27 +1,26 @@
 import AnimationSort from "../components/sortSensei/AnimationSort";
-import SortHeader from "../components/sortSensei/SortHeader";
+import SolveButton from "../components/sortSensei/SolveButton";
 import SortingTable from "../components/sortSensei/SortingTable";
 import { SortProvider } from "../contexts/SortContext";
 import styles from "../styles/sortSensei/general.module.css";
 
 const MergeSort = () => {
   return (
-    <SortProvider>
-      <SortHeader />
-      <div className={styles.container}>
-        <div>
-          <AnimationSort />
-        </div>
-        <div style={{ marginTop: "20px" }}>
-          {/* Table */}
-          <SortingTable />
-          {/* right buttons */}
-          <div></div>
-        </div>
-        {/* Solve buttons */}
+    <>
+      <div>
+        <AnimationSort />
+      </div>
+      <div style={{ marginTop: "20px" }}>
+        {/* Table */}
+        <SortingTable />
+        {/* right buttons */}
         <div></div>
       </div>
-    </SortProvider>
+      {/* Solve buttons */}
+      <div>
+        <SolveButton />
+      </div>
+    </>
   );
 };
 
