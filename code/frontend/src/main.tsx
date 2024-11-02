97bdd350245d25c4cd8fc7c22ec9479812edf86e
An Hoang <an.hoang@web.de>
2024-11-02 21:24:42 +0100

    Centered table with gray background color

--- code/frontend/src/main.tsx
+++ code/frontend/src/main.tsx
@@ -8,15 +8,12 @@ import {
 import "./index.css";
 import Home from "./pages/Home.tsx";
 import MergeSort from "./pages/MergeSort.tsx";
-import SortSensei from "./pages/SortSensei.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<Home />} />
-      <Route path="sortsensei" element={<SortSensei />}>
-        <Route path="mergesort" element={<MergeSort />} />
-      </Route>
+      <Route path="mergesort" element={<MergeSort />} />
       {/* <Route path="sortsensei/mergesort" element={<Home />} /> */}
       {/* Nested route */}
     </>
