30349051501327929baf30ea4555b9160d7c25ea
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-11-02 16:53:21 +0100

    fix link to mergesort page

--- code/frontend/src/components/homepage/Header.tsx
+++ code/frontend/src/components/homepage/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
       <div className={headerStyles["header-nav"]}>
         <div className={`${headerStyles["header-nav-list"]} ${isOpen ? headerStyles.open : ""}`}>
           <HeaderNavItem to="https://aud.ibr.cs.tu-bs.de" text="TreeTutor" />
-          <HeaderNavItem to="/sortsensei" text="SortSensei ▼" />
+          <HeaderNavItem to="sortsensei/mergesort" text="SortSensei ▼" />
           <HeaderNavItem to="https://aud.ibr.cs.tu-bs.de" text="Kontakt" />
         </div>
 
