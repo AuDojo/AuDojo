3f9e007b65966be2ad79ab520677596880a0c9a6
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-11-03 14:07:30 +0100

    add buttons and styles

--- code/frontend/src/pages/MergeSort.tsx
+++ code/frontend/src/pages/MergeSort.tsx
@@ -1,10 +1,11 @@
 import AnimationSort from "../components/sortSensei/AnimationSort";
-import SolveButton from "../components/sortSensei/SolveButton";
+import SolveButtons from "../components/sortSensei/SolveButtons";
 import SortHeader from "../components/sortSensei/SortHeader";
 import SortingTable from "../components/sortSensei/SortingTable";
 import { SortProvider } from "../contexts/SortContext";
 import styles from "../styles/sortSensei/general.module.css";
-
+import buttonsStyles from "../styles/sortSensei/Button.module.css";
+import GenerateButtons from "../components/sortSensei/GenerateButtons";
 const MergeSort = () => {
   return (
     <SortProvider>
@@ -13,16 +14,17 @@ const MergeSort = () => {
         <div>
           <AnimationSort />
         </div>
-        <div style={{ marginTop: "20px" }}>
+        <div style={{ marginTop: "20px", display: "flex", position: "relative", alignItems: "center", justifyContent: "center" }}>
           {/* Table */}
           <SortingTable />
           {/* right buttons */}
-          <div></div>
+          <div className={buttonsStyles["buttons-container"]}>
+            <GenerateButtons />
+            <SolveButtons />
+          </div>
         </div>
         {/* Solve buttons */}
-        <div>
-          <SolveButton />
-        </div>
+        <div></div>
       </div>
     </SortProvider>
   );
