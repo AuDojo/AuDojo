443b94c527b3969d4280045106ea36d5aed8278f
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-11-05 09:19:00 +0100

    make dropdown menu

--- code/frontend/src/pages/Home.tsx
+++ code/frontend/src/pages/Home.tsx
@@ -3,27 +3,20 @@ import Footer from "../components/homepage/Footer";
 import Header from "../components/homepage/Header";
 import HomeButtonList from "../components/homepage/HomeButtonList";
 import generalStyles from "../styles/homepage/general.module.css";
-import headerStyles from "../styles/homepage/Header.module.css";
 import mainStyles from "../styles/homepage/Main.module.css";
 
 const Home = () => {
   return (
     <div className={generalStyles.container}>
-      <header className={headerStyles.header}>
+      <header>
         <Header />
       </header>
       <main className={mainStyles.main}>
         <h1 className={mainStyles["main-header"]}>
           <div>AuDojo</div>
-          <img
-            className={mainStyles["audojo-logo"]}
-            src={audoLogo}
-            alt="logo of audojo"
-          />
+          <img className={mainStyles["audojo-logo"]} src={audoLogo} alt="logo of audojo" />
         </h1>
-        <p className={mainStyles["main-welcome-message"]}>
-          Willkommen bei AuDojo! Dein Dojo für Algorithmen und Datenstrukturen
-        </p>
+        <p className={mainStyles["main-welcome-message"]}>Willkommen bei AuDojo! Dein Dojo für Algorithmen und Datenstrukturen</p>
         <HomeButtonList />
       </main>
       <footer>
