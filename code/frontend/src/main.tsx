ec242432b1ef3d5cb316b49328ca676a261cc714
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-11-06 11:57:52 +0100

    add pasges and fix useState for header

--- code/frontend/src/main.tsx
+++ code/frontend/src/main.tsx
@@ -1,14 +1,11 @@
 import { createRoot } from "react-dom/client";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home.tsx";
 import MergeSort from "./pages/MergeSort.tsx";
 import QuickSort from "./pages/QuickSort.tsx";
+import SelectionSort from "./pages/SelectionSort.tsx";
+import BubbleSort from "./pages/BubbleSort.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,11 +13,11 @@ const router = createBrowserRouter(
       <Route path="/" element={<Home />} />
       <Route path="mergesort" element={<MergeSort />} />
       <Route path="quicksort" element={<QuickSort />} />
+      <Route path="selectionsort" element={<SelectionSort />} />
+      <Route path="bubblesort" element={<BubbleSort />} />
       {/* <Route path="sortsensei/mergesort" element={<Home />} /> */}
       {/* Nested route */}
     </>
   )
 );
-createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
-);
+createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
