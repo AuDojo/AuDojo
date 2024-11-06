26e403c94d54a390816a41321fdfa86af35c2a4e
An Hoang <an.hoang@web.de>
2024-11-06 18:14:02 +0100

    Added controls to the left, also key events in table

---
+++ code/frontend/src/components/sortSensei/SortTips.tsx
@@ -0,0 +1,18 @@
+import styles from "../../styles/sortSensei/SortTips.module.css";
+
+const SortTips = () => {
+  return (
+    <div className={styles["tips-container"]}>
+      <strong>Controls</strong>:
+      <div className={styles["controls-container"]}>
+        <div>↑</div>
+        <div>W</div>
+        <div>←A D→</div>
+        <div>S</div>
+        <div>↓</div>
+      </div>
+    </div>
+  );
+};
+
+export default SortTips;
