e7ec996cb05c7374bde89dba09d516860c33edbb
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-11-06 15:24:13 +0100

    pass param for fetching data

--- code/frontend/src/contexts/SortContext.tsx
+++ code/frontend/src/contexts/SortContext.tsx
@@ -6,40 +6,36 @@ export interface SortContextProps {
   step: number;
   setStep: React.Dispatch<React.SetStateAction<number>>;
   setStepsList: React.Dispatch<React.SetStateAction<number[][]>>;
+  fetchStepsList: (array: number[]) => Promise<void>;
 }
 
 // Create context with default values
-export const SortContext = createContext<SortContextProps | undefined>(
-  undefined
-);
+export const SortContext = createContext<SortContextProps | undefined>(undefined);
 
-export const SortProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
+export const SortProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [stepsList, setStepsList] = useState<number[][]>([]);
   const [step, setStep] = useState<number>(1);
 
-  useEffect(() => {
-    async function fetchStepsList() {
+  async function fetchStepsList(array: number[] = [7, 13, 5, 9, 10, 12, 1, 3, 2, 6, 25, 30, 40, 38, 32]) {
+    try {
       const response = await fetch("/api/sorting/mergesort", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          startArray: [7, 13, 5, 9, 10, 12, 1, 3, 2, 6, 25, 30, 40, 38, 32],
+          startArray: array,
         }),
       });
       const data = await response.json();
       setStepsList(JSON.parse(data).processList);
+    } catch (error) {
+      console.log("Error fetching sorting steps: ", error);
     }
-
+  }
+  useEffect(() => {
     fetchStepsList();
   }, []);
 
-  return (
-    <SortContext.Provider value={{ stepsList, step, setStep, setStepsList }}>
-      {children}
-    </SortContext.Provider>
-  );
+  return <SortContext.Provider value={{ stepsList, step, setStep, setStepsList, fetchStepsList }}>{children}</SortContext.Provider>;
 };
