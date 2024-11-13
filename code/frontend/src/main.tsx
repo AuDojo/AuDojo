e6f290a9b84bb13a0db811cc73e9920129a8ebe4
An Hoang <an.hoang@web.de>
2024-11-13 17:34:08 +0100

    Added Strict react mode, also when pressing try again, the input will reset

--- code/frontend/src/main.tsx
+++ code/frontend/src/main.tsx
@@ -1,10 +1,16 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import "./index.css";
+import BubbleSort from "./pages/BubbleSort.tsx";
 import Home from "./pages/Home.tsx";
 import MergeSort from "./pages/MergeSort.tsx";
 import QuickSort from "./pages/QuickSort.tsx";
-import BubbleSort from "./pages/BubbleSort.tsx";
 import SelectionSort from "./pages/SelectionSort.tsx";
 import Tutorial from "./pages/Tutorial.tsx";
 
@@ -16,10 +22,14 @@ const router = createBrowserRouter(
       <Route path="quicksort" element={<QuickSort />} />
       <Route path="bubblesort" element={<BubbleSort />} />
       <Route path="selectionsort" element={<SelectionSort />} />
-      <Route path="tutorial" element={<Tutorial />}/>
+      <Route path="tutorial" element={<Tutorial />} />
       {/* <Route path="sortsensei/mergesort" element={<Home />} /> */}
       {/* Nested route */}
     </>
   )
 );
-createRoot(document.getElementById("root")!).render(<RouterProvider router={router} />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
