fbd9c713e8cf15ac91ccdbf7ec9351300eaf8e3c
An Hoang <an.hoang@web.de>
2024-11-16 22:47:44 +0100

    Constants for sortType

--- code/frontend/src/constants/sorting.ts
+++ code/frontend/src/constants/sorting.ts
@@ -1,2 +1,10 @@
 export const MAX_ARRAY_SIZE = 15;
 export const INPUT_NUMBER_RANGE = { min: 1, max: 40 };
+
+export enum SortType {
+  MergeSort,
+  QuickSort,
+  BubbleSort,
+  SelectionSort,
+  UNDEFINED,
+}
