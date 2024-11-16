9ced1ad2458b30957606e68ad70707e2e2340d74
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-11-16 22:37:01 +0100

    add animation

---
+++ code/frontend/src/components/sortSensei/MergeSortGuide.tsx
@@ -0,0 +1,38 @@
+// MergeSortGuide.tsx
+import { useSortContext } from '../../hooks/sortContextHooks';
+import styles from '../../styles/sortSensei/MergeSortGuide.module.css';
+
+export const MergeSortGuide: React.FC = () => {
+  const { step, mergeRanges } = useSortContext();
+
+  const getCurrentGuideText = () => {
+    const currentRange = mergeRanges[step - 1];
+    if (!currentRange || currentRange[0] === -1) {
+      return "Starting merge sort! \nSplit array into two halves.";
+    }
+
+    const [start, end] = currentRange;
+    const length = end - start + 1;
+
+    if (length === 1) {
+      return "An array of length 1 is already sorted.";
+    } else if (length === 2) {
+      return "Compare and merge two elements in sorted order.";
+    } else {
+      if (step === 1) {
+        return "Split the array into two halves.";
+      }
+      // Add more specific messages based on the current state
+      return `Merging ... `;
+    }
+  };
+
+  return (
+    <div className={styles.guide}>
+      <h3>Merge Sort Guide</h3>
+      <div className={styles.step}>
+        <p>{getCurrentGuideText()}</p>
+      </div>
+    </div>
+  );
+};
