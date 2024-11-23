bcc7d48258c996fd1009009f9882893d49609410
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-11-23 16:25:15 +0100

    replace animationSort

--- code/frontend/src/pages/MergeSort.tsx
+++ code/frontend/src/pages/MergeSort.tsx
@@ -1,4 +1,3 @@
-import AnimationSort from "../components/sortSensei/AnimationSort";
 import GenerateButtons from "../components/sortSensei/GenerateButtons";
 import SolveButtons from "../components/sortSensei/SolveButtons";
 import SortHeader from "../components/sortSensei/SortHeader";
@@ -9,22 +8,24 @@ import buttonsStyles from "../styles/sortSensei/Button.module.css";
 import styles from "../styles/sortSensei/general.module.css";
 import tableStyles from "../styles/sortSensei/SortingTable.module.css";
 import { MergeSortGuide } from "../components/sortSensei/guide/MergeSortGuide";
-import {SortType} from '../constants/sorting';
+import { SortType } from "../constants/sorting";
+import D3SortVisualizer from "../components/sortSensei/D3SortVisualizer";
 const MergeSort = () => {
   return (
     <SortProvider>
       <SortHeader />
       <div className={styles.container}>
-        <div style={{ display: "flex" }}>
+        <div>
+          {/* <AnimationSort /> */}
+          <D3SortVisualizer />
           <MergeSortGuide />
-          <AnimationSort />
         </div>
         <div className={tableStyles["outer-table-buttons-container"]}>
           <div className={tableStyles["inner-table-buttons-container"]}>
             {/* Left Tips */}
             <SortTips />
             {/* Table */}
-            <SortingTable sortType={SortType.MergeSort}/>
+            <SortingTable sortType={SortType.MergeSort} />
             {/* right buttons */}
             <div className={buttonsStyles["buttons-container"]}>
               <GenerateButtons />
