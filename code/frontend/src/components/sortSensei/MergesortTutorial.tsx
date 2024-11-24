274e1e381f9347050dde136705d52448e6c1202c
Maginease <minseoowen@gmail.com>
2024-11-24 15:59:47 +0100

    Hilfeseite

--- code/frontend/src/components/sortSensei/MergesortTutorial.tsx
+++ code/frontend/src/components/sortSensei/MergesortTutorial.tsx
@@ -1,7 +1,15 @@
+import mergesortPseudo from "../../assets/mergeSortpseudo.png";
+import mergePseudo from "../../assets/mergepseudo.png";
+import tutorialContent from "../../styles/sortSensei/TutorialContent.module.css"
 
 const MergesortTutorial = () => {
     return (
-         <h1>Anleitung zum Mergesort!</h1>
+         
+         <div className={tutorialContent["content-container"]}> <h1>Anleitung zum Mergesort!</h1>
+         <img src={mergesortPseudo} alt="mergesort pseudocode"/>
+         <img src={mergePseudo} alt="merge pseudocode"/>
+         </div>
+        
         );
 };
 
--- code/frontend/src/components/sortSensei/TutorialHeader.tsx
+++ code/frontend/src/components/sortSensei/TutorialHeader.tsx
@@ -1,7 +1,6 @@
 import { Link, useNavigate } from "react-router-dom";
 import audojoLogo from "../../assets/logo-audojo.png";
-import headerStyles from "../../styles/sortSensei/SortHeader.module.css";
-import buttonStyles from "../../styles/sortSensei/Button.module.css";
+import headerStyles from "../../styles/sortSensei/TutorialHeader.module.css";
 
 const TutorialHeader = () => {
     const navigate = useNavigate();
@@ -14,11 +13,10 @@ const TutorialHeader = () => {
             <img className={headerStyles["header-logo-image"]} src={audojoLogo} alt="audojo logo" />
         </div>
         </Link>
-        <div className={headerStyles["header-logo-text"]}>
-            <div className={buttonStyles["generate-buttons"]}>
-                <button onClick={()=>navigate(-1)}>Back</button>
-            </div>
+        <div className={headerStyles["back-buttons"]}>
+            <button onClick={()=>navigate(-1)}>Back</button>
         </div>
+       
     </div>
   );
 };
