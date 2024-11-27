1ce0d5aa0ab6fd1dafdd827522d8beca27955630
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-11-27 20:46:12 +0100

    merge HeaderNavListItem

--- code/frontend/src/components/homepage/Header.tsx
+++ code/frontend/src/components/homepage/Header.tsx
@@ -1,39 +1,48 @@
 import { useState } from "react";
 import HeaderLogo from "./HeaderLogo";
-import HeaderNavItem from "./HeaderNavItem";
+import { Link } from "react-router-dom";
 import headerStyles from "../../styles/homepage/Header.module.css";
-
-const Header = () => {
+const HeaderNavItem = ({ to, text }: { to: string; text: string }) => {
+  return (
+    <Link className="Link" to={to}>
+      <div className={headerStyles["header-nav-list-item"]}>{text}</div>
+    </Link>
+  );
+};
+const HeaderNavItems = () => {
   const [isOpen, setOpen] = useState<boolean>(false);
   const toogleMenu = () => {
     setOpen(!isOpen);
   };
+  return (
+    <div className={headerStyles["header-nav"]}>
+      <div className={`${headerStyles["header-nav-list"]} ${isOpen ? headerStyles.open : ""}`}>
+        <HeaderNavItem to="https://aud.ibr.cs.tu-bs.de" text="TreeTutor" />
+        <div className={headerStyles["dropdown"]}>
+          <HeaderNavItem to="/mergesort" text="SortSensei ▼" />
+          <div className={headerStyles["dropdown-content"]}>
+            <HeaderNavItem to="/mergesort" text="MergeSort" />
+            <HeaderNavItem to="/quicksort" text="QuickSort" />
+            <HeaderNavItem to="/bubblesort" text="BubbleSort" />
+            <HeaderNavItem to="/selectionsort" text="SelectionSort" />
+          </div>
+        </div>
+        <HeaderNavItem to="https://aud.ibr.cs.tu-bs.de" text="Kontakt" />
+      </div>
 
+      <div className={`${headerStyles["hamburger"]} ${isOpen ? headerStyles.change : ""}`} onClick={toogleMenu}>
+        <div className={headerStyles["bar1"]}></div>
+        <div className={headerStyles["bar2"]}></div>
+        <div className={headerStyles["bar3"]}></div>
+      </div>
+    </div>
+  );
+};
+const Header = () => {
   return (
     <div className={headerStyles["header-container"]}>
       <HeaderLogo />
-
-      <div className={headerStyles["header-nav"]}>
-        <div className={`${headerStyles["header-nav-list"]} ${isOpen ? headerStyles.open : ""}`}>
-          <HeaderNavItem to="https://aud.ibr.cs.tu-bs.de" text="TreeTutor" />
-          <div className={headerStyles["dropdown"]}>
-            <HeaderNavItem to="/mergesort" text="SortSensei ▼" />
-            <div className={headerStyles["dropdown-content"]}>
-              <HeaderNavItem to="/mergesort" text="MergeSort" />
-              <HeaderNavItem to="/quicksort" text="QuickSort" />
-              <HeaderNavItem to="/bubblesort" text="BubbleSort" />
-              <HeaderNavItem to="/selectionsort" text="SelectionSort" />
-            </div>
-          </div>
-          <HeaderNavItem to="https://aud.ibr.cs.tu-bs.de" text="Kontakt" />
-        </div>
-
-        <div className={`${headerStyles["hamburger"]} ${isOpen ? headerStyles.change : ""}`} onClick={toogleMenu}>
-          <div className={headerStyles["bar1"]}></div>
-          <div className={headerStyles["bar2"]}></div>
-          <div className={headerStyles["bar3"]}></div>
-        </div>
-      </div>
+      <HeaderNavItems />
     </div>
   );
 };
