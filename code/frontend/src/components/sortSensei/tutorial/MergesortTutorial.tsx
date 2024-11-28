8d0e39fc212a248459fba6f1662bf80bd06a00ef
Maginease <minseoowen@gmail.com>
2024-11-28 17:11:45 +0100

    Beispele fuer Mergesort

--- code/frontend/src/components/sortSensei/tutorial/MergesortTutorial.tsx
+++ code/frontend/src/components/sortSensei/tutorial/MergesortTutorial.tsx
@@ -1,14 +1,22 @@
 import mergesortPseudo from "../../../assets/mergeSortpseudo.png";
 import mergePseudo from "../../../assets/mergepseudo.png";
-import tutorialContent from "../../../styles/sortSensei/TutorialContent.module.css"
+import tutorialContent from "../../../styles/sortSensei/tutorial/TutorialContent.module.css";
+import TutorialSidebar from "../../../components/sortSensei/tutorial/TutorialSidebar";
+import Visualizer from "./StepVisualizer";
 
 const MergesortTutorial = () => {
     return (
     <div className={tutorialContent["content-container"]}> 
-        <h1>Anleitung zum Mergesort!</h1>
-        <img src={mergesortPseudo} alt="mergesort pseudocode"/>
-        <img src={mergePseudo} alt="merge pseudocode"/>
+        <h1>Beispiele zum Mergesort!</h1>
+            <TutorialSidebar title="Aufklappen für Pseudocode">
+                <div className={tutorialContent["image-container"]}>
+                    <img src={mergesortPseudo} className = {tutorialContent["image"]} alt="mergesort pseudocode"/>
+                    <img src={mergePseudo} className = {tutorialContent["image"]} alt="merge pseudocode"/>
+                </div>
+            </TutorialSidebar>
+            <Visualizer/>
     </div>
+    
         
         );
 };
