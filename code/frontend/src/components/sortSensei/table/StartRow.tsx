30d22ffa4ae7c9a542bdb21e58c92ad83066fc20
An Hoang <an.hoang@web.de>
2024-12-07 23:27:16 +0100

    feat: New tableHook and when moving around in table, whole cell is selected

--- code/frontend/src/components/sortSensei/table/StartRow.tsx
+++ code/frontend/src/components/sortSensei/table/StartRow.tsx
@@ -1,5 +1,5 @@
-import { MAX_INPUT_RANGE, MIN_INPUT_RANGE } from "@constants/index";
 import { useSortContext } from "@hooks/index";
+import { useTableUtils } from "@src/hooks/useTableUtils";
 import styles from "@styles/sortSensei/SortingTable.module.css";
 import classNames from "classnames/bind";
 
@@ -7,72 +7,8 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
 const StartRow = () => {
-  const { stepsList, inputCellsRef, inputCellValues, setInputCellValues } =
-    useSortContext();
-
-  const stepList = stepsList[0];
-  /**
-   * Updates the state of the input values in the row when a user types in a new value.
-   * Checks if the input value is either empty or a valid number (less than or equal to 40)
-   * and only updates the state if the input passes this check.
-   * @param value The new value of the input
-   * @param index The index of the input in the row
-   */
-  const handleChange = (value: string, index: number): void => {
-    // Check if the input value is either empty or a valid number
-    if (
-      (Number(value) >= MIN_INPUT_RANGE && Number(value) <= MAX_INPUT_RANGE) ||
-      value === ""
-    ) {
-      const updatedValues = [...inputCellValues];
-      updatedValues[0][index] = value; // Update the specific index with new value
-      setInputCellValues(updatedValues); // Update the state
-    }
-  };
-
-  /**
-   * Focuses the input element at the given row and column index. This is used
-   * to focus the cell after pressing the Enter key to go to the next cell.
-   * @param rowIndex The index of the row
-   * @param columnIndex The index of the column
-   */
-  const focusCell = (rowIndex: number, columnIndex: number): void => {
-    const inputElement = inputCellsRef.current[rowIndex]?.[columnIndex];
-    if (inputElement) {
-      inputCellsRef.current[rowIndex][columnIndex].focus();
-    }
-  };
-
-  const handleKeyDown = (
-    event: React.KeyboardEvent<HTMLInputElement>,
-    columnIndex: number
-  ): void => {
-    const { key, shiftKey } = event;
-    if ((key === "Enter" && shiftKey) || key === "ArrowLeft") {
-      // Move to the previous field
-      if (columnIndex === 0) {
-        focusCell(0 - 1, stepList.length - 1);
-      } else {
-        focusCell(0, columnIndex - 1);
-      }
-    } else if ((key === "Enter" && !shiftKey) || key === "ArrowRight") {
-      // Move to the next input field
-      if (columnIndex === stepList.length - 1) {
-        focusCell(0 + 1, 0);
-      } else {
-        focusCell(0, columnIndex + 1);
-      }
-    } else if (key === "ArrowDown") {
-      // Move down
-      focusCell(0 + 1, columnIndex);
-    } else if (key === "ArrowUp") {
-      // Move up
-      focusCell(0 - 1, columnIndex);
-    } else if (key === "Escape") {
-      // Unfocus the input field
-      (event.target as HTMLInputElement).blur();
-    }
-  };
+  const { stepsList, inputCellsRef, inputCellValues } = useSortContext();
+  const { handleCellChange, handleCellKeyDown } = useTableUtils();
 
   return (
     <div className={cx("start-row-container")}>
@@ -91,9 +27,9 @@ const StartRow = () => {
               }
             }}
             onChange={
-              (event) => handleChange(event.target.value, index) // Update value
+              (event) => handleCellChange(event.target.value, 0, index) // Update value
             }
-            onKeyDown={(event) => handleKeyDown(event, index)}
+            onKeyDown={(event) => handleCellKeyDown(event, 0, index)}
           />
         </div>
       ))}
--- code/frontend/src/components/sortSensei/table/TableRow.tsx
+++ code/frontend/src/components/sortSensei/table/TableRow.tsx
@@ -1,8 +1,8 @@
-import { MAX_INPUT_RANGE, MIN_INPUT_RANGE, SortType } from "@constants/index";
+import { SortType } from "@constants/index";
 import { useSortContext } from "@hooks/index";
+import { useTableUtils } from "@src/hooks/useTableUtils";
 import styles from "@styles/sortSensei/SortingTable.module.css";
 import classNames from "classnames/bind";
-import React from "react";
 import Tooltip from "../Tooltip";
 
 interface RowProps {
@@ -18,16 +18,10 @@ const cx = classNames.bind(styles);
  */
 const TableRow = ({ rowIndex }: RowProps) => {
   // Access the context values
-  const {
-    stepsList,
-    step,
-    mergeRanges,
-    inputCellsRef,
-    sortTypeRef,
-    inputCellValues,
-    cellValidation,
-    setInputCellValues,
-  } = useSortContext();
+  const { stepsList, step, mergeRanges, inputCellsRef, sortTypeRef, inputCellValues, cellValidation } =
+    useSortContext();
+
+  const { handleCellChange, handleCellKeyDown } = useTableUtils();
 
   const sortType = sortTypeRef.current;
   let mergeRange: [number, number];
@@ -44,74 +38,6 @@ const TableRow = ({ rowIndex }: RowProps) => {
   }
   const stepList = stepsList[rowIndex];
   const validation = cellValidation[rowIndex];
-
-  /**
-   * Updates the state of the input values in the row when a user types in a new value.
-   * Checks if the input value is either empty or a valid number (less than or equal to 40)
-   * and only updates the state if the input passes this check.
-   * @param value The new value of the input
-   * @param columnIndex The index of the input in the row
-   */
-  const handleChange = (value: string, columnIndex: number): void => {
-    // Check if the input value is either empty or a valid number
-    if (
-      (Number(value) >= MIN_INPUT_RANGE && Number(value) <= MAX_INPUT_RANGE) ||
-      value === ""
-    ) {
-      const updatedValues = [...inputCellValues];
-      updatedValues[rowIndex][columnIndex] = value; // Update the specific index with new value
-      setInputCellValues(updatedValues); // Update the state
-    }
-  };
-
-  /**
-   * Focuses the input element at the given row and column index. This is used
-   * to focus the cell after pressing the Enter key to go to the next cell.
-   * @param rowIndex The index of the row
-   * @param columnIndex The index of the column
-   */
-  const focusCell = (rowIndex: number, columnIndex: number): void => {
-    const inputElement = inputCellsRef.current[rowIndex]?.[columnIndex];
-    if (inputElement) {
-      inputCellsRef.current[rowIndex][columnIndex].focus();
-    }
-  };
-
-  /**
-   * Handles pressed keys. WASD to move in the table, 'Escape' to unfocus the current input field
-   * @param event The keyboard event
-   * @param columnIndex The index of the column
-   */
-  const handleKeyDown = (
-    event: React.KeyboardEvent<HTMLInputElement>,
-    columnIndex: number
-  ): void => {
-    const { key, shiftKey } = event;
-    if ((key === "Enter" && shiftKey) || key === "ArrowLeft") {
-      // Move to the previous field
-      if (columnIndex === 0) {
-        focusCell(rowIndex - 1, stepList.length - 1);
-      } else {
-        focusCell(rowIndex, columnIndex - 1);
-      }
-    } else if ((key === "Enter" && !shiftKey) || key === "ArrowRight") {
-      // Move to the next input field
-      if (columnIndex === stepList.length - 1) {
-        focusCell(rowIndex + 1, 0);
-      } else {
-        focusCell(rowIndex, columnIndex + 1);
-      }
-    } else if (key === "ArrowDown") {
-      // Move down
-      focusCell(rowIndex + 1, columnIndex);
-    } else if (key === "ArrowUp") {
-      // Move up
-      focusCell(rowIndex - 1, columnIndex);
-    } else if (key === "Escape") {
-      // Unfocus the input field
-      (event.target as HTMLInputElement).blur();
-    }
-  };
 
   /**
    * Checks if the given column index is within the current merge range.
@@ -140,26 +66,14 @@ const TableRow = ({ rowIndex }: RowProps) => {
           >
             <input
               className={cx("cell-input", {
-                "in-merge-range":
-                  sortType === SortType.MergeSort &&
-                  isInMergeRange(columnIndex) &&
-                  rowIndex < step,
+                "in-merge-range": sortType === SortType.MergeSort && isInMergeRange(columnIndex) && rowIndex < step,
                 "merge-range-start":
-                  sortType === SortType.MergeSort &&
-                  columnIndex === mergeRange[0] &&
-                  rowIndex < step,
-                "merge-range-end":
-                  sortType === SortType.MergeSort &&
-                  columnIndex === mergeRange[1] &&
-                  rowIndex < step,
+                  sortType === SortType.MergeSort && columnIndex === mergeRange[0] && rowIndex < step,
+                "merge-range-end": sortType === SortType.MergeSort && columnIndex === mergeRange[1] && rowIndex < step,
                 correct: validation[columnIndex] === true, // Correct user input
                 incorrect: validation[columnIndex] === false, // Incorrect user input
               })}
-              value={
-                rowIndex < step
-                  ? num || ""
-                  : inputCellValues[rowIndex][columnIndex]
-              } // Show number for index below step, or value from state otherwise
+              value={rowIndex < step ? num || "" : inputCellValues[rowIndex][columnIndex]} // Show number for index below step, or value from state otherwise
               readOnly={rowIndex < step} // Make read-only if index is below current step
               ref={(el) => {
                 // Assign the input element to the appropriate cell in the ref
@@ -168,11 +82,9 @@ const TableRow = ({ rowIndex }: RowProps) => {
                 }
               }}
               onChange={
-                (event) =>
-                  rowIndex >= step &&
-                  handleChange(event.target.value, columnIndex) // Update value
+                (event) => rowIndex >= step && handleCellChange(event.target.value, rowIndex, columnIndex) // Update value
               }
-              onKeyDown={(event) => handleKeyDown(event, columnIndex)}
+              onKeyDown={(event) => handleCellKeyDown(event, rowIndex, columnIndex)}
               placeholder={rowIndex === 1 && columnIndex === 0 ? "Edit" : ""}
             />
           </Tooltip>
