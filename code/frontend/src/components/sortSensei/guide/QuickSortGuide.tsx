706b7c230551896c11a947f16e5d9955e7121397
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-12-11 20:07:30 +0100

    feat: quick sort guide

--- code/frontend/src/components/sortSensei/guide/QuickSortGuide.tsx
+++ code/frontend/src/components/sortSensei/guide/QuickSortGuide.tsx
@@ -3,27 +3,42 @@ import { useSortContext } from "@hooks/index";
 import styles from "@styles/sortSensei/MergeSortGuide.module.css";
 
 const QuickSortGuide: React.FC = () => {
-  const { step, mergeRanges } = useSortContext();
+  const { step, pivotElement, stepsList } = useSortContext();
 
   const getCurrentGuideText = () => {
-    const currentRange = mergeRanges[step - 1];
-    if (!currentRange || currentRange[0] === -1) {
+    if (!pivotElement || pivotElement.length === 0) {
       return "Starting Quick sort! ";
     }
 
-    const [start, end] = currentRange;
-    const length = end - start + 1;
-
-    if (length === 1) {
-      return "An array of length 1 is already sorted.";
-    } else if (length === 2) {
-      return "...";
-    } else {
-      if (step === 1) {
-        return "...";
-      }
-      // Add more specific messages based on the current state
-      return ` ... `;
+    if (step === 1) {
+      return (
+        <>
+          Choose <b>last element {stepsList[0][stepsList[0].length - 1]}</b> as pivot element.
+        </>
+      );
+    } else if (step >= 2) {
+      const prevArray = stepsList[step - 2];
+      const pivot = prevArray[pivotElement[step - 1][0]];
+      const currentArray = stepsList[step - 1];
+      const nextPivot = step !== stepsList.length ? currentArray[pivotElement[step][0]] : 0;
+      if (!nextPivot)
+        return (
+          <>
+            <p>
+              <b> Table: Mark the used pivot element "{pivot}"</b>
+            </p>
+            <br />
+            Sorting is complete!
+          </>
+        );
+      return (
+        <>
+          After partitioning, all elements smaller than the {pivot} are on its left, and larger on the right.
+          <b> Choose {nextPivot}</b> as <b>next</b> pivot element.
+          <br />
+          <b>Table: Mark the used pivot element "{pivot}"</b>
+        </>
+      );
     }
   };
 
