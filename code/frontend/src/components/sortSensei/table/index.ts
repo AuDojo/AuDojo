90e4d3b652c66b13365ddf2a3f52b2387273f7a0
An Hoang <an.hoang@web.de>
2024-12-11 15:38:42 +0100

    feat/refactor: separate tableCell logic into own file, mark pivot element in table

--- code/frontend/src/components/sortSensei/table/index.ts
+++ code/frontend/src/components/sortSensei/table/index.ts
@@ -1,4 +1,5 @@
 export { default as IndexRow } from "./IndexRow";
 export { default as SortingTable } from "./SortingTable";
 export { default as StartRow } from "./StartRow";
+export { default as TableCell } from "./TableCell";
 export { default as TableRow } from "./TableRow";
