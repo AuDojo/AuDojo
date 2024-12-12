279c0f7bea9f84016797685342858fe918581d2b
Maginease <minseoowen@gmail.com>
2024-12-12 14:35:21 +0100

    minor:index.ts for tutorialData

---
+++ code/frontend/src/components/sortSensei/tutorial/tutorialData/index.ts
@@ -0,0 +1,4 @@
+export { default as bubbleSortSteps } from "./bubbleSortData";
+export { default as selectionSortSteps } from "./selectionSortData";
+export { default as quickSortSteps } from "./quickSortData";
+export { default as mergeSortSteps } from "./mergeSortData";
