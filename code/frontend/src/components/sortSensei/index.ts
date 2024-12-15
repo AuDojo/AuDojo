35ec1a7c09a5fb9e1fe638165b6db79451ac96b6
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-12-15 15:08:17 +0100

    remove Sorttip

--- code/frontend/src/components/sortSensei/index.ts
+++ code/frontend/src/components/sortSensei/index.ts
@@ -2,7 +2,6 @@ export { default as AnimationSort } from "./AnimationSort";
 export { default as D3SortVisualizer } from "./D3SortVisualizer";
 export { default as Points } from "./Points";
 export { default as SortHeader } from "./SortHeader";
-export { default as SortTips } from "./SortTips";
 export { default as Tooltip } from "./Tooltip";
 
 export * from "./buttons";
