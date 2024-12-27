4dbc1abe339ee9953e93aa0f1c49d4f1db8db384
Maxman002 <maximo.stooge@gmail.com>
2024-12-27 17:09:25 +0100

    Removed unused routing code in server.ts

--- code/backend/server.ts
+++ code/backend/server.ts
@@ -1,21 +1,4 @@
-/*
-//import dotenv from 'dotenv';
 import express, { Express, Request, Response } from "express";
-
-const app: Express = express();
-
-const PORT = process.env.PORT || 5001;
-
-app.get("/", (req: Request, res: Response) => {
-  // root route http://localhost:5001
-  res.send("Hello World!");
-});
-
-app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
-*/
-
-import express, { Express, Request, Response } from "express";
-import path from "path";
 
 const app: Express = express();
 const port = 5001;
@@ -23,20 +6,12 @@ const bodyParser = require("body-parser");
 
 // make sure the right sortingRouting.* is required
 const env = process.env.NODE_ENV; // 'development' oder 'production'
-const sortingRouting = require(
-  env === 'development' ? './sortingRouting.ts' : './sortingRouting.js'
-);
+// const sortingRouting = require(env === "development" ? "./sortingRouting.ts" : "./sortingRouting.js");
+const sortingRouting = require("./sortingRouting");
 
-// make express deliver static frontend pages
-app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'public')));
 
-app.use(express.static(path.join(__dirname, '..', '..', 'frontend', 'dist')));
 app.use(bodyParser.json());
 app.use("/api/sorting", sortingRouting);
-
-app.get("/", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, "..", "frontend", "index.html")); //our main page frontend/index.html is loaded
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
