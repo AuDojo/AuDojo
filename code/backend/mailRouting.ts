d58eb4f56091b19cbd8f12f7fb3767b64de806db
Maxman002 <maximo.stooge@gmail.com>
2024-12-27 18:19:04 +0100

    Added the ability to now send mails via code
    Added mail.ts for the sending of the mail
    Added mailRouting.ts for the post request to do so

--- code/backend/mailRouting.ts
+++ code/backend/mailRouting.ts
@@ -1,10 +1,25 @@
 import { Request, Response } from "express";
+import { sendMail } from "./mail";
+import dotenv from "dotenv";
 
 const express = require("express");
 const router = express.Router();
 
 router.post("/", (req: Request, res: Response) => {
-  console.log("Geklappt!!");
+  console.log("---------- Mail wird gesendet ----------");
+
+  let subject = req.body.subject;
+  let text = req.body.text;
+
+  if (process.env.MAIL_USERNAME == undefined) {
+    res.send("Error!! Mail-Account not found");
+    console.log("Error!");
+    return;
+  } else {
+    sendMail(process.env.MAIL_USERNAME, process.env.MAIL_USERNAME, subject, text);
+    console.log("Geklappt!!");
+  }
+
   res.send("hello world");
 });
 
