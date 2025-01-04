f99bed11a54f7fc6b547bd34285fd9ae538d9ca9
Anomatomato <55243106+anomatomato@users.noreply.github.com>
2025-01-04 13:15:46 +0100

    Feat/selection table (#166)
    * Finished selected elements in table

--- code/frontend/src/features/sortSensei/table/tableCell/TableCell.tsx
+++ code/frontend/src/features/sortSensei/table/tableCell/TableCell.tsx
@@ -15,8 +15,17 @@ interface TableCellProps {
 const cx = classNames.bind(styles);
 
 const TableCell = ({ rowIndex, columnIndex }: TableCellProps): JSX.Element => {
-  const { stepsList, step, mergeRanges, sortTypeRef, inputCellValues, inputCellsRef, cellValidation, pivotElement } =
-    useSortContext();
+  const {
+    stepsList,
+    step,
+    mergeRanges,
+    sortTypeRef,
+    inputCellValues,
+    inputCellsRef,
+    cellValidation,
+    pivotElement,
+    selectionElement,
+  } = useSortContext();
 
   const { handleCellChange, handleCellKeyDown } = useTableUtils();
 
@@ -58,11 +67,11 @@ const TableCell = ({ rowIndex, columnIndex }: TableCellProps): JSX.Element => {
     };
   }, [rowIndex, columnIndex, stepsList, inputCellValues, cellValidation, mergeRanges, sortTypeRef, pivotElement]);
 
-  // const isSelected = () => {
-  //   if (sortTypeRef.current !== SortType.SelectionSort) {
-  //     return;
-  //   }
-  // };
+  const isSelected = () => {
+    if (sortTypeRef.current === SortType.SelectionSort && rowIndex < step && rowIndex !== 0) {
+      return columnIndex === selectionElement[rowIndex - 1] || columnIndex === rowIndex - 1;
+    }
+  };
 
   return (
     <Tooltip
@@ -81,6 +90,7 @@ const TableCell = ({ rowIndex, columnIndex }: TableCellProps): JSX.Element => {
           correct: cellData.validation === true,
           incorrect: cellData.validation === false,
           "pivot-cell": cellData.isPivot && rowIndex < step,
+          "selected-cell": isSelected(),
         })}
         value={rowIndex < step ? cellData.num || "" : cellData.inputCellValue}
         readOnly={rowIndex < step}
