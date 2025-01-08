e9729e4d33d9f23c17e7b5bef88f779ce7256e5d
An Hoang <an.hoang@web.de>
2025-01-08 17:18:49 +0100

    Add lazy loading

---
+++ code/frontend/src/constants/paths.ts
@@ -0,0 +1,15 @@
+export const paths = {
+  home: "/",
+  sortSensei: {
+    mergeSort: "/mergesort",
+    quickSort: "/quicksort",
+    bubbleSort: "/bubblesort",
+    selectionSort: "/selectionsort",
+  },
+  tutorial: "/tutorial",
+  kontakt: "/kontakt",
+  legal: {
+    datenschutz: "/datenschutz",
+    impressum: "/impressum",
+  },
+} as const;
