58882eb6d553c1ec2bae2de1a83a41d6ae3a9f12
An Hoang <an.hoang@web.de>
2025-01-27 12:53:37 +0100

    Disable eslint rule: only-export-components

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -44,6 +44,7 @@ export default tseslint.config({
     ...pluginQuery.configs["flat/recommended"][0].rules, // Integrate TanStack Query rules
     ...prettierConfig.rules, // Disables ESLint rules that conflict with Prettier
     "prettier/prettier": "warn",
+    // "react-refresh/only-export-components": ["off", { allowConstantExport: true }],
     "import/no-restricted-paths": [
       "error",
       {
