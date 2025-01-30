03ca9dcba33b9770cf03d9adbfa188ea9fedf1e6
An Hoang <an.hoang@web.de>
2025-01-30 20:26:36 +0100

    feat: browser language gets detected now and stored in localStorage

--- frontend/src/translation/i18n.ts
+++ frontend/src/translation/i18n.ts
@@ -1,12 +1,19 @@
 import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import LanguageDetector, { DetectorOptions } from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+const detectionOptions: DetectorOptions = {
+  order: ["localStorage", "navigator"],
+  caches: ["localStorage"],
+};
 
 i18n
   .use(Backend)
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
-    lng: "de",
+    detection: detectionOptions,
     fallbackLng: "de",
     interpolation: {
       escapeValue: false,
