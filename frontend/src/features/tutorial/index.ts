c395ddcddc7838f408a6e986684b2f8027cfe830
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2025-02-02 20:14:15 +0100

    remove unused/duplicated components/files

--- frontend/src/features/tutorial/index.ts
+++ frontend/src/features/tutorial/index.ts
@@ -1,4 +1,3 @@
-export { default as TutorialHeader } from "./components/header/TutorialHeader";
 export { default as RickRollTroll } from "./components/rickRollTroll/RickRollTroll";
 export { default as TutorialSidebar } from "./components/sidebar/TutorialSidebar";
 export { default as StepVisualizer } from "./components/stepVisualizer/StepVisualizer";
