bc34624f75bd1370642579faa490f583a1041a15
An Hoang <an.hoang@web.de>
2025-02-03 23:29:52 +0100

    fix: sortSensei routing

--- backend/server.ts
+++ backend/server.ts
@@ -7,21 +7,23 @@ const bodyParser = require("body-parser");
 
 // make sure the right sortingRouting.* is required
 const env = process.env.NODE_ENV; // 'development' oder 'production'
-const sortingRouting = require(env === "development" ? "./sortingRouting.ts" : "./SortSensei/sortingRouting.js");
+const sortingRouting = require(env === "development"
+  ? "./SortSensei/sortingRouting.ts"
+  : "./SortSensei/sortingRouting.js");
 const mailRouting = require("./Mail/mailRouting");
 
-
 // make express deliver static frontend pages
-app.use(express.static(path.join(__dirname,'..' ,'..', 'frontend', 'public')));
-app.use(express.static(path.join(__dirname,'..' ,'..', 'frontend', 'dist')));
+if (env === "production") {
+  app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")));
+  app.use(express.static(path.join(__dirname, "..", "..", "frontend", "public")));
+  app.get("*", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "..", "..", "frontend", "dist", "index.html")); //our main page frontend/index.html is loaded
+  });
+}
 
 app.use(bodyParser.json());
 app.use("/api/sorting", sortingRouting);
 app.use("/api/mail", mailRouting);
-
-app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname ,"..", ".." , "frontend","dist", "index.html")); //our main page frontend/index.html is loaded
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
