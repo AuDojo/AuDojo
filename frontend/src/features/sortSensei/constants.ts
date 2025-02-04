ca64d17fc78cb0adb6f85c9b499dda27fcc970f1
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2025-02-04 21:38:51 +0100

    allow max_iput_range to 99

--- frontend/src/features/sortSensei/constants.ts
+++ frontend/src/features/sortSensei/constants.ts
@@ -4,7 +4,7 @@ export const MAX_ARRAY_SIZE = 15;
 export const MIN_ARRAY_SIZE = 2;
 
 export const MIN_INPUT_RANGE = 1;
-export const MAX_INPUT_RANGE = 40;
+export const MAX_INPUT_RANGE = 99;
 
 export const SortTypes: Record<string, SortType> = {
   MergeSort: "mergesort",
