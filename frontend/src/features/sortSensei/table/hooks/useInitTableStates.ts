b529abae451ad48552d401261a9aaf1d569b7d39
An Hoang <an.hoang@web.de>
2025-02-04 01:23:14 +0100

    feat: useMemo for useInitTable

--- frontend/src/features/sortSensei/table/hooks/useInitTableStates.ts
+++ frontend/src/features/sortSensei/table/hooks/useInitTableStates.ts
@@ -1,23 +1,26 @@
 import { useSortContext } from "@/features/sortSensei/context/SortContext";
+import { useMemo } from "react";
 
 export const useInitTableStates = () => {
   const { processList } = useSortContext();
 
-  if (!processList || processList.length === 0) {
-    return {
-      inputCellValues: [],
-      cellValidation: [],
-      inputCells: [],
-    };
-  }
+  return useMemo(() => {
+    if (!processList || processList.length === 0) {
+      return {
+        inputCellValues: [],
+        cellValidation: [],
+        inputCells: [],
+      };
+    }
 
-  const inputCellValues = processList.map((step, index) =>
-    index === 0 ? step.map(String) : new Array<string>(step.length).fill("")
-  );
+    const inputCellValues = processList.map((step, index) =>
+      index === 0 ? step.map(String) : new Array<string>(step.length).fill("")
+    );
 
-  const cellValidation = processList.map((step) => new Array<null>(step.length).fill(null));
+    const cellValidation = processList.map((step) => new Array<null>(step.length).fill(null));
 
-  const inputCells = processList.map((step) => new Array<null>(step.length).fill(null));
+    const inputCells = processList.map((step) => new Array<null>(step.length).fill(null));
 
-  return { inputCellValues, cellValidation, inputCells };
+    return { inputCellValues, cellValidation, inputCells };
+  }, [processList]); // Memoize based on processList
 };
