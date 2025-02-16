2e0fce078cc2e22025a28639164c4971f5992b34
An Hoang <an.hoang@web.de>
2025-02-16 19:12:23 +0100

    fix(header): false css import

--- frontend/src/components/header/LinkItem/LinkItem.tsx
+++ frontend/src/components/header/LinkItem/LinkItem.tsx
@@ -1,7 +1,7 @@
+import { Spinner } from "@/components/ui/spinner";
 import { JSX, Suspense } from "react";
 import { Link } from "react-router-dom";
-import { Spinner } from "../../ui/spinner";
-import headerStyle from "./Header.module.css";
+import headerStyles from "../Header.module.css";
 
 const LinkItem = ({
   className,
@@ -15,7 +15,7 @@ const LinkItem = ({
   icon?: JSX.Element;
 }) => {
   return (
-    <Link className={headerStyle[className]} to={to}>
+    <Link className={headerStyles[className]} to={to}>
       {text}
       <Suspense fallback={<Spinner size="sm" />}>{icon}</Suspense>
     </Link>
