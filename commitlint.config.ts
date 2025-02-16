2c63d03a50b6f80c3a99bc114502f2275d52bd6a
An Hoang <an.hoang@web.de>
2025-02-16 15:06:09 +0100

    dev: add dev as commit-type to config

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,4 @@
+import conventional from "@commitlint/config-conventional";
 import type { UserConfig } from "@commitlint/types";
 import { RuleConfigSeverity } from "@commitlint/types";
 
@@ -10,11 +11,7 @@ const Configuration: UserConfig = {
     "body-max-line-length": [RuleConfigSeverity.Error, "always", 800],
     // TODO Add Scope Enum here
     // 'scope-enum': [RuleConfigSeverity.Error, 'always', ['yourscope', 'yourscope']],
-    "type-enum": [
-      RuleConfigSeverity.Error,
-      "always",
-      ["feat", "fix", "docs", "style", "revert", "refactor", "perf", "test", "build", "ci", "chore"],
-    ],
+    "type-enum": [RuleConfigSeverity.Error, "always", [...conventional.rules["type-enum"][2], "dev"]],
   },
   helpUrl: "https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional",
 };
