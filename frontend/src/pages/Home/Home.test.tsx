b182f3568a4ae8ef512b014995838f8a75f9eb56
An Hoang <an.hoang@web.de>
2025-02-18 20:02:36 +0100

    test(home): test home buttons on click

--- frontend/src/pages/Home/Home.test.tsx
+++ frontend/src/pages/Home/Home.test.tsx
@@ -1,18 +1,47 @@
-import { screen } from "@testing-library/react";
+import { paths } from "@/config";
+import { act, screen } from "@testing-library/react";
+import { UserEvent } from "@testing-library/user-event";
 import { renderWithRouter } from "tests/testUtils";
 import Home from "./Home";
 
-describe("Homepage", () => {
-  it("should render the home page correctly", () => {
-    // ARRANGE: Render the home component
-    renderWithRouter(<Home />);
+describe("<Home>", () => {
+  let user: UserEvent;
 
-    // ASSERT
-    // Test if header and logo are rendered
-    expect(screen.getByRole("heading")).toHaveTextContent("AuDojo");
-    expect(screen.getByAltText("logo of audojo")).toBeInTheDocument();
+  beforeEach(() => {
+    const renderResult = renderWithRouter(<Home />, { route: paths.home });
+    user = renderResult.user;
+  });
+
+  it("renders correctly with all main elements", () => {
+    // Test if header is rendered
+    const header = screen.getByRole("heading", { name: /AuDojo/i });
+    expect(header).toBeInTheDocument();
+
+    // Logo is rendered
+    const logo = screen.getByAltText(/logo of audojo/i);
+    expect(logo).toBeInTheDocument();
 
     // Test if welcome message is rendered
-    expect(screen.getByRole("paragraph")).toHaveTextContent("welcomeMessage");
+    const welcomeMessage = screen.getByText(/welcomeMessage/i);
+    expect(welcomeMessage).toBeInTheDocument();
+  });
+
+  // Test navigation with home buttons
+  const homeButtonTests: { name: RegExp | string; path: string }[] = [
+    { name: /Sort Sensei/i, path: paths.mergeSort },
+    { name: /Tree Tutor/i, path: paths.treeTutor },
+  ];
+
+  homeButtonTests.forEach(({ name, path }) => {
+    it(`navigates to ${path} when ${name} button is clicked`, async () => {
+      const homeButton = screen.getByRole("link", { name });
+      expect(homeButton).toBeInTheDocument();
+
+      // Click home button
+      await act(async () => user.click(homeButton));
+
+      // Assert that the URL has changed
+      expect(window.location.pathname).toBe(path);
+    });
   });
 });
