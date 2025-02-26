b88c014e62b40c88dcb2c44e8df744a61343855e
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2025-02-26 22:14:17 +0100

    feat(test): mock global object (ResizeObserver)

--- frontend/tests/setup.ts
+++ frontend/tests/setup.ts
@@ -23,6 +23,16 @@ vi.mock("react-i18next", () => ({
   Trans: ({ children }: { children: React.ReactNode }) => children, // Mock Trans component
 }));
 
+//! Mock globals objects, API, vars
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(), // vi.fn() just tracks how the function is called,
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+
+// change the value of global variable / replace a global variable with a mock version
+vi.stubGlobal("ResizeObserver", ResizeObserverMock);
+
 //! Cleanup after every test
 afterEach(() => {
   // screen.debug();
