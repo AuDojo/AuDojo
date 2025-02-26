42f29a8c7e17780c30c3e52304f9236867a52135
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2025-02-26 10:39:37 +0100

    feat: extend type of img for homebutton, not only pgn but also Component (for svg)

--- frontend/src/features/home/homeButtons/HomeButton.tsx
+++ frontend/src/features/home/homeButtons/HomeButton.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import styles from "./HomeButtons.module.css";
+import { ReactNode } from "react";
 
 interface HomeButtonProps {
   link: string;
   header: string;
-  img: string;
+  img: string | ReactNode;
   text: string;
 }
 
@@ -12,7 +13,13 @@ const HomeButton = ({ link, header, img, text }: HomeButtonProps) => {
   return (
     <Link to={link} className={styles["home-button-link"]}>
       <h2 className={styles["home-button-header"]}>{header}</h2>
-      <img className={styles["home-button-img"]} src={img} alt={`${header} illustration`} />
+      {typeof img === "string" ? (
+        <img className={styles["home-button-img"]} src={img} alt={`${header} illustration`} />
+      ) : (
+        img
+      )}
+      {/* <img className={styles["home-button-img"]} src={img} alt={`${header} illustration`} />
+      <div></div> */}
       <p className={styles["home-button-text"]}>{text}</p>
     </Link>
   );
