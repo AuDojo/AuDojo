7330ff5cecf466e882033c9787652b8984ebd413
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2025-02-28 15:50:02 +0100

    refactor: remove comment of old code

--- frontend/src/features/home/homeButtons/HomeButtons.tsx
+++ frontend/src/features/home/homeButtons/HomeButtons.tsx
@@ -1,18 +1,15 @@
 import { paths } from "@/config/paths";
 import { useTranslation } from "react-i18next";
-// import sortingPng from "./assets/sorting.png";
 import HomeButton from "./HomeButton";
 import styles from "./HomeButtons.module.css";
-import Sorting from "./assets/Sorting";
-import AVLTree from "./assets/AVLTree";
+import Sorting from "./components/Sorting";
+import AVLTree from "./components/AVLTree";
 
 const HomeButtons = () => {
   const { t } = useTranslation("home");
   return (
     <>
       <div className={styles["home-buttons-container"]}>
-        {/* <HomeButton link={paths.mergeSort} header="Sort Sensei" img={sortingPng} text={t("sortSenseiButton")} /> */}
-        {/* <HomeButton link={paths.treeTutor} header="Tree Tutor" img={treePng} text={t("treetutorButton")} /> */}
         <HomeButton link={paths.mergeSort} header="Sort Sensei" img={<Sorting />} text={t("sortSenseiButton")} />
         <HomeButton link={paths.treeTutor} header="Tree Tutor" img={<AVLTree />} text={t("treetutorButton")} />
       </div>
