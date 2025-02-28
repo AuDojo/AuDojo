8be632348a8c18f4d572ff8465bb9259cb577b84
Trang Nguyen <106863467+trang-ngn@users.noreply.github.com>
2025-02-28 15:54:14 +0100

    feat(homebutton): create own svg for treetutor button tree (#285)
    * refactor: remove unused commit
    
    * refactor: use classname bind
    
    * feat: demo homebutton treetutor
    
    * feat: static treetutor svg
    
    * feat(homebutton): animate tree operations
    
    * refactor: remove comment of old code
    
    * refactor: import constants
    
    ---------
    
    Co-authored-by: Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>

--- frontend/src/features/home/homeButtons/components/Sorting.tsx
+++ frontend/src/features/home/homeButtons/components/Sorting.tsx
@@ -4,20 +4,10 @@ import style from "../HomeButtons.module.css";
 import useScales from "@/features/sortSensei/components/sortVisualizer/hooks/useScales";
 import { createBubbleSortData } from "@/features/sortSensei/components/sortVisualizer/utils/createBubbleSortData";
 import { useResizeObserver } from "@/hooks/useResizeObserver";
+import classNames from "classnames/bind";
+import { demoArray, demoProcess, demoBubbleElements, durationSorting } from "./constants";
 
-// demo data to show bubble sort animation on homebutton
-const array: number[] = [4, 3, 1, 2, 6, 5];
-const process = [
-  [4, 3, 1, 2, 6, 5],
-  [3, 4, 1, 2, 6, 5],
-  [3, 1, 4, 2, 6, 5],
-  [3, 1, 2, 4, 6, 5],
-  [3, 1, 2, 4, 5, 6],
-  [1, 3, 2, 4, 5, 6],
-  [1, 2, 3, 4, 5, 6],
-];
-const bubbleElements = [0, 1, 2, 4, 0, 1];
-const durationSwapElements = 600;
+const cx = classNames.bind(style);
 
 /** Component that describes SortSensei home-button */
 const Sorting = () => {
@@ -33,15 +23,15 @@ const Sorting = () => {
 
   /** specific data of bubblesort: {currentArray, numSteps, i, bubbleElements} */
   const data = createBubbleSortData({
-    currentArray: process[step],
-    numSteps: process.length,
+    currentArray: demoProcess[step],
+    numSteps: demoProcess.length,
     i: step,
-    bubbleElements,
+    bubbleElements: demoBubbleElements,
   });
 
   const { xScale, yScale } = useScales({
     containerSize,
-    sharedArray: array,
+    sharedArray: demoArray,
   });
 
   useEffect(() => {
@@ -67,7 +57,7 @@ const Sorting = () => {
     if (isHovering.current) {
       bars
         .transition()
-        .duration(durationSwapElements)
+        .duration(durationSorting)
         .attr("class", style["unsorted-bar"])
         .attr("transform", (d) => `translate(${xScale(d.index)}, 0)`);
     }
@@ -89,8 +79,8 @@ const Sorting = () => {
     isHovering.current = true;
     clearTimer();
     timerRef.current = setInterval(() => {
-      setStep((prev) => (prev + 1) % process.length);
-    }, durationSwapElements);
+      setStep((prev) => (prev + 1) % demoProcess.length);
+    }, durationSorting);
   };
   const handleStopHovering = () => {
     isHovering.current = false;
@@ -101,7 +91,7 @@ const Sorting = () => {
   return (
     <svg
       ref={svgRef}
-      className={style["sortsensei-img"]}
+      className={cx("home-button-img", "sortsensei-img")}
       onMouseEnter={handleHovering}
       onMouseLeave={handleStopHovering}
     ></svg>
