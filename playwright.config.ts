af3db5d70e45da948b7cdfc9e33b7faa92d78e91
An Hoang <an.hoang@web.de>
2025-03-03 01:56:43 +0100

    ci: back to 2 workers

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,8 +25,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* 1 parallel workes in CI */
-  workers: process.env.CI ? 1 : undefined,
+  /* 2 parallel workes in CI */
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? "blob" : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
