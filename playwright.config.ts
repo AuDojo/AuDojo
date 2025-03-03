c6826d040d5408fa187b70e8160cde1a7fc6af44
An Hoang <an.hoang@web.de>
2025-03-03 01:08:12 +0100

    ci: add sharding to workflow e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   /* 2 parallel workes in CI */
   workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI ? "blob" : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   // use: {
   /* Base URL to use in actions like `await page.goto('/')`. */
