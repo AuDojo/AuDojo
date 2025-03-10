e5d6702318808ca782d9483eb817faf0192041d8
Maginease <minseoowen@gmail.com>
2025-03-10 12:34:05 +0100

    Beispiel fuer L-rotate

--- frontend/src/pages/Tutorial/TreeTutor/LTutorial.tsx
+++ frontend/src/pages/Tutorial/TreeTutor/LTutorial.tsx
@@ -6,56 +6,75 @@ import { TUT_TREETUTOR_DEFAULTPATH, TUT_TREETUTOR_PATHS } from "../utilConstants
 
 const RED = "rgb(248, 83, 152)";
 
-const t1: Tree = {
-  value: 1,
-  children: [{ value: 2 }, { value: null }],
+const s1: Tree = {
+  value: 10,
+  children: [{ value: null }, { value: 20 }],
+};
+
+const s2: Tree = {
+  value: 10,
   color: RED,
+  children: [{ value: null }, { value: 20 }],
 };
 
-const t3: Tree = {
-  value: 1,
-  children: [t1, { value: 3 }],
+const s3: Tree = {
+  value: 10,
+  children: [{ value: null }, { value: 20, color: RED }],
 };
 
-const t4: Tree = {
-  value: 1,
-  children: [t3, t3],
+const s4: Tree = {
+  value: 10,
+  children: [{ value: null }, { value: 20, children: [{ value: null }, { value: 30 }] }],
 };
 
-const t5: Tree = {
-  value: 1,
-  children: [
-    { value: 2, children: [{ value: null }, { value: 4 }] },
-    { value: 3, children: [{ value: 5 }, { value: null }] },
-  ],
+const s5: Tree = {
+  value: 10,
+  color: RED,
+  children: [{ value: null }, { value: 20, color: RED, children: [{ value: null }, { value: 30, color: RED }] }],
 };
 
-const t2: Tree = {
-  value: 1,
-  children: [t5, t4],
+const s6: Tree = {
+  value: 20,
+  children: [{ value: 10 }, { value: 30 }],
 };
 
 const data: TreeSteps = {
   steps: [
     {
-      description: "blah blah",
-      tree: t1,
+      description: "insert 30",
+      tree: s1,
     },
     {
-      description: "blah blah2",
-      tree: t2,
+      description: "10<30",
+      tree: s2,
     },
     {
-      description: "blah blah3",
-      tree: t3,
+      description: "20<30",
+      tree: s3,
     },
     {
-      description: "blah blah4",
-      tree: t4,
+      description: "insert 30",
+      tree: s4,
     },
     {
-      description: "blah blah5",
-      tree: t5,
+      description: "10 has right child of depth 3 on right, 0(null) on left=>unbalanced",
+      tree: s5,
+    },
+    {
+      description: "choose the deepest path to unbalanced vertices",
+      tree: s5,
+    },
+    {
+      description: "Rotate 10,20,30 to left",
+      tree: s5,
+    },
+    {
+      description: "Rotate 15,16,17 to left",
+      tree: s6,
+    },
+    {
+      description: "Balanced!",
+      tree: s6,
     },
   ],
 };
