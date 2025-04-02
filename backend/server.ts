598813859590097a3f4d91aba42b0504a0cecc75
Anomatomato <55243106+anomatomato@users.noreply.github.com>
2025-04-02 20:37:27 +0200

    feat: serving static files with nginx instead of express (#415)
    * feat: serving static files with nginx instead of express
    
    * test: remove /projects/audojo from e2e

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,7 @@
 import bodyParser from "body-parser";
-import express, { Express, Request, Response } from "express";
-import path from "path";
+import express, { Express } from "express";
 
 const app: Express = express();
-const base_url = "";
 const port = process.env.PORT || 5001;
 const env = process.env.NODE_ENV || "development"; // 'development' oder 'production'
 
@@ -11,22 +9,11 @@ const env = process.env.NODE_ENV || "development"; // 'development' oder 'produc
 const sortingRouting = require(`./SortSensei/sortingRouting.${env === "development" ? "ts" : "js"}`);
 const mailRouting = require("./Mail/mailRouting");
 
-// make express deliver static frontend pages
-if (env === "production") {
-  const frontendPath = path.join(__dirname, "..", "..", "frontend");
-  // console.log("produciton is true");
-  app.use(base_url, express.static(path.join(frontendPath, "dist")));
-
-  app.get(`${base_url}/*`, (req: Request, res: Response) => {
-    res.sendFile(path.join(frontendPath, "dist", "index.html")); //our main page frontend/index.html is loaded
-  });
-}
-
 app.use(bodyParser.json());
 
 // API Routes
-app.use(`${base_url}/api/sorting`, sortingRouting);
-app.use(`${base_url}/api/mail`, mailRouting);
+app.use("/api/sorting", sortingRouting);
+app.use("/api/mail", mailRouting);
 
 // Start server
 app.listen(port, () => {
